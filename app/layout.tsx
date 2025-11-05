31d5691c724aa8b10119eb661f7dc93c1e16b789
damianGG <damiangolon@gmail.com>
2025-11-05 10:26:21 +0100

    Revert "Merge pull request #13 from damianGG/copilot/fix-31580048-1087430769-631a5e23-fe33-4a9a-bd6f-aa46bc36e148"
    This reverts commit 7de8568337e7a7102a94afa8286cbae05ba00b25, reversing
    changes made to f8fbfc40135bd5919e064f56566f28a5f9c13b95.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from 'next'
 // import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import 'leaflet/dist/leaflet.css'
 
 // const geist = Geist({ 
 //   subsets: ["latin"],
