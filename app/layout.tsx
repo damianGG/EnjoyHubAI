9ee5226477c4fc290b936302f9e375cf48ea6a15
Damian Golon <damiangolon@gmail.com>
2026-09-09 10:19:59 +0200

    brand: use EnjoyHub icon and heavier wordmark font

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,11 @@ export const metadata: Metadata = {
     template: '%s | EnjoyHub',
   },
   description: 'Znajdź atrakcje, sprawdź dostępne terminy i zarezerwuj miejsce online.',
+  icons: {
+    icon: '/enjoyhub-icon.svg',
+    shortcut: '/enjoyhub-icon.svg',
+    apple: '/placeholder-logo.svg',
+  },
 }
 
 export default function RootLayout({
@@ -36,14 +41,14 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
         {/* Leaflet CSS for map functionality */}
         <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          integrity="sha256-p4NxAoJB9BMY="
           crossOrigin=""
         />
         {/* Preconnect to Cloudinary for faster image loading */}
