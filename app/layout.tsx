77f2c95e5d1afcdc149ae0d62158cd646a2f3286
Damian Golon <damiangolon@gmail.com>
2026-09-09 10:20:21 +0200

    fix: restore Leaflet stylesheet integrity

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
         <link
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJB9BMY="
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
         {/* Preconnect to Cloudinary for faster image loading */}
