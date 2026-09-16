9e0c38f99a711d507f86e179a489ed091db331c9
Damian Golon <damiangolon@gmail.com>
2026-09-16 09:16:26 +0200

    feat: enable Sentry build integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
@@ -60,4 +62,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: true,
+  widenClientFileUpload: true,
+  disableLogger: true,
+})
